32328df05b2c9692314421d3d6256087f2460bcb
Pieter404 <pieterprawn@gmail.com>
2025-10-14 12:56:00 +0200

    added the report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import num_words, num_char
+from stats import num_words, num_char, sortList
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -8,7 +8,17 @@ def get_book_text(filepath):
 def main():
     word_count = num_words(get_book_text("/home/sarin/projects/bookbot/books/frankenstein.txt"))
     letter_count = num_char(get_book_text("/home/sarin/projects/bookbot/books/frankenstein.txt"))
-    print(f"{word_count} words found in the document")
-    print(letter_count)
+    sorted_chars = sortList(letter_count)
+    print(f"============= BOOKBOT ============")
+    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"----------- Word Count -----------")
+    print(f"Found {word_count} total words")
+    print(f"--------- Character Count --------")
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+    print(f"=============== END ==============")
 
 main()
--- stats.py
+++ stats.py
@@ -13,3 +13,16 @@ def num_char(book_text):
         else:
             letterCount[char] = 1
     return letterCount
+
+def sort_on(char):
+    return char["num"]
+    
+def sortList(letterCount):
+    sortedList = []
+    for letter in letterCount:
+        letterDic = {}
+        letterDic["char"] = letter
+        letterDic["num"] = letterCount[letter]
+        sortedList.append(letterDic)
+    sortedList.sort(reverse=True, key=sort_on)
+    return sortedList
